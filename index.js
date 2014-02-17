ec8b744c66a02f9b2ce9956a7800fc063d81de11
fredsterss <fredsters_s@me.com>
2014-02-17 15:05:44 -0800

    simple component setup

---
+++ index.js
@@ -0,0 +1,12 @@
+var d3 = require('d3');
+
+/**
+ * Expose BarChart
+ */
+module.exports = BarChart;
+
+function BarChart (el) {
+  if (!(this instanceof BarChart)) return new BarChart(el);
+  console.log('hai werld');
+  console.log(d3);
+}
