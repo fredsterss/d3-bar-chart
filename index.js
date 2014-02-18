f3ae005fc42f586fb8f55054f1b8544e26f91528
fredsterss <fredsters_s@me.com>
2014-02-17 21:14:40 -0800

    add axes, moar dummy data and gradient coloringz

--- index.js
+++ index.js
@@ -24,33 +24,59 @@ function BarChart (selector) {
  * @return {BarChart}
  */
 BarChart.prototype.makeVerticalSvgChart = function (data) {
-  var width = 960
-    , height = 500
+  var margin = {top: 20, right: 30, bottom: 30, left: 40}
+    , width = 960 - margin.left - margin.right
+    , height = 500 - margin.top - margin.bottom
     , barWidth = width / data.length;
 
+  var x = d3.scale.ordinal()
+    .domain(data.map(function(d) { return d.name; }))
+    .rangeRoundBands([0, width], .1);
+
   var y = d3.scale.linear()
-      .domain([0, d3.max(data, function(d) { return d.value; })])
-      .range([height, 0]);
+    .domain([0, d3.max(data, function(d) { return d.value; })])
+    .range([height, 0]);
 
-  var chart = d3.select(this.selector)
-      .attr("width", width)
-      .attr("height", height);
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
 
-  var bar = chart.selectAll("g")
-        .data(data)
-      .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10);
 
-  bar.append("rect")
-    .attr("y", function(d) { return y(d.value); })
-    .attr("height", function(d) { return height - y(d.value); })
-    .attr("width", barWidth -1);
+  var svg = d3.select(this.selector).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  bar.append("text")
-    .attr("x", barWidth / 2)
-    .attr("y", function(d) { return y(d.value) + 3; })
-    .attr("dy", ".75em")
-    .text(function(d) { return d.name; });
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("fill", function (d, i) {
+        return "rgb(150," + Math.floor(i * 255 / data.length) + ",170)"; })
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.name); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); });
 
   return this;
 }
