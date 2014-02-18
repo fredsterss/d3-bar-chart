bfb6d7469f66674c0a75e7c1c602b61672c7c0dc
fredsterss <fredsters_s@me.com>
2014-02-17 19:15:41 -0800

    svgz

--- index.js
+++ index.js
@@ -7,8 +7,58 @@ module.exports = BarChart;
 
 function BarChart (selector) {
   if (!(this instanceof BarChart)) return new BarChart(selector);
-  var data = [4, 8, 15, 16, 23, 42];  
-  makeDivChart(selector, data);
+  var data = [4, 8, 15, 16, 23, 42];
+
+  var dataMD = [
+    { name: "Locke", value: 4 },
+    { name: "Reyes", value: 8 },
+    { name: "Ford", value: 15 },
+    { name: "Jarrah", value: 16 },
+    { name: "Shephard", value: 23 },
+    { name: "Kwon", value: 42 }
+  ];
+
+  // makeDivChart(selector, data);
+  makeSvgChart(selector, dataMD);
+}
+
+/**
+ * Makes an SVG Chart
+ * @param  {String} selector css selector string
+ * @param  {Array} data      graph dataz
+ */
+function makeSvgChart (selector, data) {
+  var width = 420
+    , barHeight = 20;
+
+  // map input domain to output range
+  var x = d3.scale.linear()
+      .domain([0, d3.max(data, function(d) { return d.value; })])
+      .range([0, width]);
+
+  // select chart container and set width and height
+  var chart = d3.select(selector)
+      .attr("width", width)
+      .attr("height", barHeight * data.length);
+
+  // use data join to create a 'g' element for each
+  // data point, then translate the 'g' element vertically
+  var bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  // Append a 'rect' to each 'g' and style it properly
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .attr("height", barHeight - 1);
+
+  // append a 'text' el to the 'g' and style and set content
+  bar.append("text")
+      .attr("x", function(d) { return x(d.value) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function (d) { return d.value; });
 }
 
 /**
